1d5825a31361f12e48e03963357e077250d2ef07
1111PP <1156912249@qq.com>
2024-10-19 15:09:17 +0800

    change

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
     <div className="absolute w-full z-10">
       <div className="flex justify-between container mx-auto text-white p-8 items-center">
         <Link className="text-3xl font-bold" href="/">
-          Home
+          主页
         </Link>
         <div className="text-xl space-x-4">
           {linkData.map((link) => (
